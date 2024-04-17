b0a8afbe8ec51f2bfc86b4ce79dccf608977456c
Obemik <misaperepecaj@gmail.com>
2024-04-17 20:04:40 +0300

    Lab ex1 done

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "                Name       : \"The war and the peace\"\n";
+    cout << "                Avtor      :   L.N. Tolstoj\n";
+    cout << "                Izdatelstvo:  Piter\n";
+    cout << "                Pages      : 500\n" << endl;
+
+    return 0;
+}
